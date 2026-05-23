3f8f5383524b3b6fddd572098d5002120e5e42f3
Sang Min Kim <the.ricochet.software@gmail.com>
2026-05-23 01:48:50 -0700

    thumbnail image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,34 @@ export default defineNuxtConfig({
 
   app: {
     head: {
+      meta: [
+        { property: "og:type", content: "website" },
+        {
+          property: "og:url",
+          content: "https://my-little-vendors.vercel.app/",
+        },
+        { property: "og:title", content: "My Little Vendors" },
+        {
+          property: "og:description",
+          content: "밴쿠버 한인 소상공인 디렉토리",
+        },
+        {
+          property: "og:image",
+          content:
+            "https://my-little-vendors.vercel.app/images/preview/thumbnail.png",
+        },
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: "My Little Vendors" },
+        {
+          name: "twitter:description",
+          content: "밴쿠버 한인 소상공인 디렉토리",
+        },
+        {
+          name: "twitter:image",
+          content:
+            "https://my-little-vendors.vercel.app/images/preview/thumbnail.png",
+        },
+      ],
       script: [
         {
           src: "https://www.googletagmanager.com/gtag/js?id=G-BCEGFB9CDG",
